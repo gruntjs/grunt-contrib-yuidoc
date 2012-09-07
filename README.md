740d6e960ef5128b6f53949a344cafa93bc0cbfb
Tyler Kellen <tyler@sleekcode.net>
2012-09-07 15:54:48 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# grunt-contrib-yuidoc
+> Compile YUIDoc Documentation (part of the [grunt-contrib](/gruntjs/grunt-contrib) collection).  Submitted by [George Pantazis](/gcpantazis).
+
+## Getting Started
+Install this grunt plugin next to your project's [grunt.js gruntfile][getting_started] with: `npm install grunt-contrib-less`
+
+Then add this line to your project's `grunt.js` gruntfile:
+
+```javascript
+grunt.loadNpmTasks('grunt-contrib-yuidoc');
+```
+
+[grunt]: https://github.com/cowboy/grunt
+[getting_started]: https://github.com/cowboy/grunt/blob/master/docs/getting_started.md
+
+### Overview
+
+Inside your `grunt.js` file add a section named `yuidoc`. [Visit the YUIDoc project home](http://yui.github.com/yuidoc/) for more information on YUIDocs and commenting syntax.
+
+#### Parameters
+
+##### options ```object```
+
+This controls how this task operates and should contain key:value pairs, see options below.
+
+#### Options
+
+Options correlate to [YUIDoc config objects](http://yui.github.com/yuidoc/args/index.html).
+
+##### paths (required) ```string|array```
+
+What directory should be scanned to generate doc data.
+
+##### outdir (required) ```string```
+
+Where to save the documentation.
+
+#### Config Example
+
+``` javascript
+yuidoc: {
+  compile: {
+    "name": "Project Name",
+    "description": "Project Description",
+    "version": "1.0.2",
+    "url": "http://projecturl.com/",
+    options: {
+      paths: "path/to/source/code/",
+      outdir: "where/to/save/docs/"
+    }
+  }
+}
+```
---
+++ grunt.js
@@ -0,0 +1,89 @@
+/*
+ * grunt-contrib-yuidoc
+ * http://gruntjs.com/
+ *
+ * Copyright (c) 2012 George Pantazis, contributors
+ * Licensed under the MIT license.
+ * https://github.com/gruntjs/grunt-contrib-yuidoc/blob/master/LICENSE-MIT
+ */
+
+module.exports = function(grunt) {
+  'use strict';
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: {
+      name: "grunt-contrib-yuidoc",
+      version: "0.1.0"
+    },
+
+    lint: {
+      all: ['grunt.js', 'tasks/*.js', '<config:nodeunit.tasks>']
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        boss: true,
+        eqnull: true,
+        node: true,
+        es5: true
+      }
+    },
+    // Before generating any new files, remove any previously-created files.
+    clean: {
+      test: ['tmp']
+    },
+    // Configuration to be run (and then tested).
+    yuidoc: {
+      compileA: {
+        "name": "Grunt Test",
+        "description": "Grunt Test Description",
+        "version": "1.2.1",
+        "url": "http://test.com/",
+        options: {
+          paths: "test/fixtures/app/",
+          outdir: "tmp/yuidoca/"
+        }
+      },
+      compileB: {
+        "name": "Grunt Test",
+        "description": "Grunt Test Description",
+        "version": "1.2.1",
+        "url": "http://test.com/",
+        options: {
+          paths: [
+            "test/fixtures/app/",
+            "test/fixtures/otherapp/"
+          ],
+          outdir: "tmp/yuidocb/"
+        }
+      }
+    },
+    // Unit tests.
+    nodeunit: {
+      tasks: ['test/*_test.js']
+    }
+  });
+
+  // Actually load this plugin's task(s).
+  grunt.loadTasks('tasks');
+
+  // The clean plugin helps in testing.
+  grunt.loadNpmTasks('grunt-contrib-clean');
+
+  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+  // plugin's task(s), then test the result.
+  grunt.renameTask('test', 'nodeunit');
+  grunt.registerTask('test', 'clean yuidoc nodeunit');
+
+  // By default, lint and run all tests.
+  grunt.registerTask('default', 'lint test');
+
+};
